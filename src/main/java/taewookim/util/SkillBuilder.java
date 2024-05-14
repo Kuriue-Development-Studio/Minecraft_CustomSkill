97290c2ce5f9906627fbe754a78d913a2dd5d290
catking562 <taewookim562@gmail.com>
2024-05-14 17:01:06 +0900

    op권한을 가진 플레이어가 명령어로 스킬을 생성할 수 있습니다.

--- src/main/java/taewookim/util/SkillBuilder.java
+++ src/main/java/taewookim/util/SkillBuilder.java
@@ -1,5 +1,7 @@
 package taewookim.util;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import taewookim.CustomSkillPlugin;
 import taewookim.skills.Skill;
 
@@ -7,28 +9,52 @@ import java.util.ArrayList;
 
 public class SkillBuilder {
 
-    private final ArrayList<Skill> skills;
+    private final ArrayList<SkillTypes> skills;
+    private final ArrayList<ElementTypes> elements;
+    private final ArrayList<Integer> powers;
 
     public SkillBuilder() {
         skills = new ArrayList<>();
+        elements = new ArrayList<>();
+        powers = new ArrayList<>();
     }
 
     public SkillBuilder(int skillcount) {
         skills = new ArrayList<>(skillcount);
+        elements = new ArrayList<>(skillcount);
+        powers = new ArrayList<>(skillcount);
     }
 
-    public void addSkill(Skill skill) {
-        skills.add(skill);
+    public void addSkill(SkillTypes skilltype, ElementTypes elementType, int power) {
+        skills.add(skilltype);
+        elements.add(elementType);
+        powers.add(power);
     }
 
-    public void build() {
+    public void build(SkillOwner owner) {
         if(skills.isEmpty()) {
             return;
         }
-        for(int i = 0; i<skills.size()-1; i++) {
-            skills.get(i).setNextSkill(skills.get(i+1));
+        Skill firstskill = skills.get(0).create(owner, elements.get(0), powers.get(0));
+        Skill lastskill = firstskill;
+        for(int i = 1; i<skills.size(); i++) {
+            Skill newskill = skills.get(i).create(owner, elements.get(i), powers.get(i));
+            lastskill.setNextSkill(newskill);
+            lastskill = newskill;
         }
-        CustomSkillPlugin.plugin.addSkill(skills.get(0));
+        CustomSkillPlugin.plugin.addSkill(firstskill);
+    }
+
+    public void build(Player p) {
+        SkillOwner owner = new SkillOwner();
+        owner.setOwner(p);
+        build(owner);
+    }
+
+    public void build(Location loc) {
+        SkillOwner owner = new SkillOwner();
+        owner.setLocation(loc.getX(), loc.getY(), loc.getZ());
+        build(owner);
     }
 
 }
