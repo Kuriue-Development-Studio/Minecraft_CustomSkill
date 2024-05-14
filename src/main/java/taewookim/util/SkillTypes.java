d7dd9e081cf87f112e0c54dd1091adff0947ddfa
catking562 <taewookim562@gmail.com>
2024-05-14 17:20:51 +0900

    SkillTypes에 SKILL_PAIN 추가

--- src/main/java/taewookim/util/SkillTypes.java
+++ src/main/java/taewookim/util/SkillTypes.java
@@ -1,12 +1,14 @@
 package taewookim.util;
 
 import taewookim.skills.Skill;
+import taewookim.skills.attackskill.SkillPain;
 import taewookim.skills.utilskill.SkillTarget;
 
 public enum SkillTypes {
 
     SKILL(Skill.class),
-    SKILLTARGET(SkillTarget.class),
+    SKILL_TARGET(SkillTarget.class),
+    SKILL_PAIN(SkillPain.class),
     ;
 
     private final Class<? extends Skill> clz;
