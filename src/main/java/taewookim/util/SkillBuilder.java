9d9356d7c49920d0859652784de05678b734b96b
BuildTools <unconfigured@null.spigotmc.org>
2024-07-14 17:21:53 +0900

    ItemStack으로 Skill을 빌드 할 수 있습니다.

--- src/main/java/taewookim/util/SkillBuilder.java
+++ src/main/java/taewookim/util/SkillBuilder.java
@@ -2,6 +2,10 @@ package taewookim.util;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import taewookim.CustomSkillPlugin;
 import taewookim.skills.Skill;
 
@@ -23,6 +27,28 @@ public class SkillBuilder {
         skills = new ArrayList<>(skillcount);
         elements = new ArrayList<>(skillcount);
         powers = new ArrayList<>(skillcount);
+    }
+
+    public SkillBuilder(ItemStack i) {
+        if(!i.hasItemMeta()) {
+            skills = new ArrayList<>();
+            elements = new ArrayList<>();
+            powers = new ArrayList<>();
+            return;
+        }
+        PersistentDataContainer container = i.getItemMeta().getPersistentDataContainer();
+        int size = 0;
+        if(container.has(CustomSkillPlugin.itemskill, PersistentDataType.INTEGER)) {
+            size = container.get(CustomSkillPlugin.itemskill, PersistentDataType.INTEGER);
+        }
+        skills = new ArrayList<>(size);
+        elements = new ArrayList<>(size);
+        powers = new ArrayList<>(size);
+        for(int j = 0; j<size; j++) {
+            skills.add(SkillTypes.valueOf(container.get(CustomSkillPlugin.skilltypes.get(j), PersistentDataType.STRING)));
+            elements.add(ElementTypes.valueOf(container.get(CustomSkillPlugin.elementtypes.get(j), PersistentDataType.STRING)));
+            powers.add(container.get(CustomSkillPlugin.powertypes.get(j), PersistentDataType.INTEGER));
+        }
     }
 
     public void addSkill(SkillTypes skilltype, ElementTypes elementType, int power) {
